72682b6001365fc62022a7d69c1ce756a66541ab
Ilan Shavit <ilan.shavit@gmail.com>
2025-09-10 22:08:13 +0300

    Updates

--- etc/skel/.bin/arch/41-sddm-change-wallpaper.sh
+++ etc/skel/.bin/arch/41-sddm-change-wallpaper.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sudo mkdir -p feh  /usr/share/sddm/sddm-backgroungs/
+sudo mkdir -p /usr/share/sddm/sddm-backgroungs/
 sudo cp my-wallpaper.jpg  /usr/share/sddm/sddm-backgroungs/
+sudo cp theme.conf.user /usr/share/sddm/themes/arcolinux-simplicity/
