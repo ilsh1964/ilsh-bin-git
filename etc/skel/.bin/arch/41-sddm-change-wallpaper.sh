7ba3a42255fb844d76bdd85dbcf085ee1a2debf4
Ilan Shavit <ilan.shavit@gmail.com>
2025-09-10 22:02:17 +0300

    updates...

--- etc/skel/.bin/arch/41-sddm-change-wallpaper.sh
+++ etc/skel/.bin/arch/41-sddm-change-wallpaper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mkdir -p feh  /usr/share/sddm/sddm-backgroungs/
-cp my-wallpaper.jpg  /usr/share/sddm/sddm-backgroungs/
+sudo mkdir -p feh  /usr/share/sddm/sddm-backgroungs/
+sudo cp my-wallpaper.jpg  /usr/share/sddm/sddm-backgroungs/
