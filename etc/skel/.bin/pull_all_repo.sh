55bff83427e332dcc4cfe8e97681e77847520560
Ilan Shavit <ilan.shavit@gmail.com>
2025-09-13 16:40:51 +0300

    Updates

--- etc/skel/.bin/pull_all_repo.sh
+++ etc/skel/.bin/pull_all_repo.sh
@@ -9,12 +9,6 @@ cd ../edu-dot-files
 git pull
 cd ../edu-shells
 git pull
-cd ../edu-variety-config
-git pull
-cd ../nemesis_repo
-git pull
-cd ../kirodubes
-git pull
 cd ..
 
 # Mench repo
@@ -24,7 +18,7 @@ cd ../
 
 
 # dwm-setup
-cd dwm-setup
+cd drewgrif/dwm-setup
 git pull
 
 popd
